e5375a6dd4a067da9415d73433380ea99a708480
RakeshMudidana <rakeshpavanmudidana@gmail.com>
2025-06-16 18:14:06 +0000

    initial commit

--- todo-cli/__tests__/first.js
+++ todo-cli/__tests__/first.js
@@ -1,6 +1,38 @@
 /* eslint-disable no-undef */
-describe("First test suite", () => {
-  test("First case", () => {
-    expect(true).toBe(true);
+const todoList = require("../todo");
+
+const {
+  all,
+  add,
+  markAsComplete,
+  // overdue,
+  // dueToday,
+  // dueLater,
+  // toDisplayableList,
+} = todoList();
+
+describe("TodoList test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      dueDate: new Date().toLocaleDateString("en-CA"),
+      completed: false,
+    });
+  });
+
+  test("Should add new todo", () => {
+    const TodoItemCount = all.length;
+    add({
+      title: "Test todo",
+      dueDate: new Date().toLocaleDateString("en-CA"),
+      completed: false,
+    });
+    expect(all.length).toBe(TodoItemCount + 1);
+  });
+
+  test("Should mark todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
   });
 });
